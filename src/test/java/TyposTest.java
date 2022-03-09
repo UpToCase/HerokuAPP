bc3a2daf0af20fd4fd82ed75c325d539faa77ade
UpToCase <99139170+UpToCase@users.noreply.github.com>
2022-03-09 21:22:31 +0100

    div-*

--- src/test/java/TyposTest.java
+++ src/test/java/TyposTest.java
@@ -13,7 +13,7 @@ public class TyposTest extends BaseTest {
     public void spellingCheckTest() {
         driver.get("https://the-internet.herokuapp.com/typos");
         WebElement textToBeSpellingCheck =
-                driver.findElement(By.xpath("//div[@class='example']/p[2]"));
+                driver.findElement(By.xpath("//*[@class='example']/p[2]"));
         assertEquals(textToBeSpellingCheck.getText(),
                 "Sometimes you'll see a typo, other times you won't.",
                 "there is a mistake in this text");
