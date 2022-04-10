91f0245ee156e4b05042718aa0fbc1cc1be51d31
UpToCase <99139170+UpToCase@users.noreply.github.com>
2022-04-10 14:26:52 +0200

    create FileUploadTest

---
+++ src/test/java/FileUploadTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+import java.io.File;
+
+import static org.testng.Assert.assertEquals;
+
+public class FileUploadTest extends BaseTest{
+
+    @Test
+    public void fileUploadTest() {
+        driver.get("https://the-internet.herokuapp.com/upload");
+        File movieFileForDownload = new File("src/test/resources/text.html");
+        driver.findElement(By.id("file-upload")).sendKeys(movieFileForDownload.getAbsolutePath());
+        driver.findElement(By.id("file-submit")).click();
+        wait.until(ExpectedConditions.attributeContains(By.id("uploaded-files"),
+                "class", "panel text-center"));
+        assertEquals(driver.findElement(By.cssSelector(".example")).getText(),
+                "File Uploaded!\ntext.html",
+                "file do not been uploaded");
+    }
+}
+
