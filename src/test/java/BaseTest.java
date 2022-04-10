63e71a77863c324551dc4e30a9714bfb0f9526c3
UpToCase <99139170+UpToCase@users.noreply.github.com>
2022-04-10 12:01:39 +0200

    add WebDriverWait

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,8 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -8,15 +10,16 @@ import java.util.concurrent.TimeUnit;
 
 public abstract class BaseTest {
     WebDriver driver;
+    WebDriverWait wait;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver",
-                "src/test/resources/chromedriver");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
-        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterMethod(alwaysRun = true)
